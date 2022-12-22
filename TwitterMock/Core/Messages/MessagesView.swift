63662e2d952d644f54ef0be3771ed30800da6413
Sada Kshirsagar <sadashiv@gmail.com>
2022-12-21 22:14:00 -0500

    Finished MainTab Setup, still need to do profile

---
+++ TwitterMock/Core/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  TwitterMock
+//
+//  Created by Sada Kshirsagar on 12/21/22.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Messages")
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
---
+++ TwitterMock/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsView.swift
+//  TwitterMock
+//
+//  Created by Sada Kshirsagar on 12/21/22.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Notifications")
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
