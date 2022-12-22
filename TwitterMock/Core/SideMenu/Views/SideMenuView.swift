0e1a7ea99ff9074432c1378e3e5f8116b7b49f65
Sada Kshirsagar <sadashiv@gmail.com>
2022-12-22 14:31:56 -0500

    Finished Side Menu Layout

--- TwitterMock/Core/SideMenu/Views/SideMenuView.swift
+++ TwitterMock/Core/SideMenu/Views/SideMenuView.swift
@@ -10,21 +10,40 @@ import SwiftUI
 struct SideMenuView: View {
     var body: some View {
         
-        VStack(alignment: .leading) {
-            Circle()
-                .frame(width: 48, height: 48)
-            
-            VStack(alignment: .leading, spacing: 4) {
-                Text("Bruce Wayne")
-                    .font(.headline)
+        VStack(alignment: .leading, spacing: 32) {
+            VStack(alignment: .leading) {
+                Circle()
+                    .frame(width: 48, height: 48)
                 
-                Text("@batman")
-                    .font(.caption)
-                    .foregroundColor(.gray)
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Bruce Wayne")
+                        .font(.headline)
+                    
+                    Text("@batman")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                
+                UserStatsView()
+                    .padding(.vertical)
+            }
+            .padding(.leading)
+            
+            ForEach(SideMenuViewModel.allCases, id: \.rawValue) { option in
+                HStack(spacing: 16) {
+                    Image(systemName: option.imageName)
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                    Text(option.title)
+                        .font(.subheadline)
+                    
+                    Spacer ()
+                }
+                .frame(height: 40)
+                .padding(.horizontal)
             }
             
-            UserStatsView()
-                .padding(.vertical)
+            Spacer ()
         }
     }
 }
