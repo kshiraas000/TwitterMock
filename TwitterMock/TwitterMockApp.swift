88af927d68cfaf86d19a338ee35bb0d77f6ac254
Sada Kshirsagar <sadashiv@gmail.com>
2022-12-21 21:05:09 -0500

    Initial Commit

---
+++ TwitterMock/TwitterMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterMockApp.swift
+//  TwitterMock
+//
+//  Created by Sada Kshirsagar on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
