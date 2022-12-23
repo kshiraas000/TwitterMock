855e6f11f15e83a3748b53ca460204f672877de7
Sada Kshirsagar <sadashiv@gmail.com>
2022-12-23 11:53:08 -0500

    finished login ui

---
+++ TwitterMock/Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationView.swift
+//  TwitterMock
+//
+//  Created by Sada Kshirsagar on 12/23/22.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
--- TwitterMock/TwitterMockApp.swift
+++ TwitterMock/TwitterMockApp.swift
@@ -12,7 +12,7 @@ struct TwitterMockApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ContentView()
+                LoginView()
             }
         }
     }
